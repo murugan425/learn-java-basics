f12e5981b26ece91cd66b49dcb2266355b09faa4
ATPIAD1\atpsmnn <mnagarajan@atpco.net>
2019-08-16 05:39:21 -0400

    Create a lambda expression function and pass that as a argument to a method.

---
+++ java-basics/src/com/tamil/learn/java/basics/inheritance/UpperConcat.java
@@ -0,0 +1,5 @@
+package com.tamil.learn.java.basics.inheritance;
+
+public interface UpperConcat {
+	public String upperAndConcat(String s1, String s2);
+}
--- java-basics/src/com/tamil/learn/java/features/eight/lambda/LambdaExample4.java
+++ java-basics/src/com/tamil/learn/java/features/eight/lambda/LambdaExample4.java
@@ -3,17 +3,54 @@
  */
 package com.tamil.learn.java.features.eight.lambda;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+import com.tamil.learn.java.basics.inheritance.UpperConcat;
+import com.tamil.learn.java.basics.vo.Employee;
+
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author atpsmnn
  *
  */
+@Slf4j
 public class LambdaExample4 {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
+		RandomStringUtils randStr = new RandomStringUtils();
+		
+		List<Employee> employees = IntStream.range(0, 10).
+				mapToObj(o -> new Employee(randStr.randomAlphabetic(10), Integer.valueOf(randStr.randomNumeric(2)), LocalDate.now())) .collect(Collectors.toList()); 
+		log.info(employees.size() + " Employees Generated");	
+		employees.forEach(emp -> System.out.println(emp));
+		
+		//Pass function as an argument to a method in a lambda expression
+		/*
+		UpperConcat uc = new UpperConcat() {		
+			@Override
+			public String upperAndConcat(String empName, String empDob) {
+				return "Name: " + empName.toUpperCase() + " / Date of Birth: " + empDob;
+			}
+		};	
+		*/
+		
+		//Modified the function to a lambda expression
+		UpperConcat uc = (empName, empDob) -> "Name: " + empName.toUpperCase() + " / Date of Birth: " + empDob;
+	
+		employees.forEach(emp -> System.out.println(printEmployee(uc, emp)));	
+		
 	}
 
+	public final static String printEmployee(UpperConcat uc, Employee emp) {
+		return uc.upperAndConcat(emp.getName(), emp.getDob().toString());
+	}
 }
