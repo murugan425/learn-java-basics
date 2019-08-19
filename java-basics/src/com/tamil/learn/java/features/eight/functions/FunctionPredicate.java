3a3c91d02a7891d0afa3734ea56fb5b5bc6b337f
ATPIAD1\atpsmnn <mnagarajan@atpco.net>
2019-08-19 07:52:15 -0400

    Using BiFunctions which accepts two inputs and produce one result

--- java-basics/src/com/tamil/learn/java/features/eight/functions/FunctionPredicate.java
+++ java-basics/src/com/tamil/learn/java/features/eight/functions/FunctionPredicate.java
@@ -6,6 +6,7 @@ package com.tamil.learn.java.features.eight.functions;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
@@ -27,6 +28,11 @@ public class FunctionPredicate {
 	Function<Employee, String> formatEmpNoAge = (emp) -> 
 		String.format("%s dob is %s and he earns %02d $ per hour.", 
 			emp.getName().toUpperCase(), emp.getDob(), emp.getSalary());
+		
+	//BiFunction can accept two inputs and produce one result which helps to chain multiple functions infact.
+	//We can even give the output of one function as the input of BiFunction
+	BiFunction<String, Employee, String> formatEmpWithAge = (empStr, emp) -> 
+		empStr.concat(String.format("[Age: %d]", Period.between(emp.getDob(), LocalDate.now()).getYears()));
 	
 	//Predicates helps to minimize the complex multiple if conditions that can be passed as arguments now.
 	public void filterEmployeesByAge(List<Employee> employees, Predicate<Employee> filterCond) {
@@ -40,7 +46,7 @@ public class FunctionPredicate {
 	public void filterEmployeesByAge(List<Employee> employees, Predicate<Employee> minAge, Predicate<Employee> maxAge) {
 		employees.forEach(emp -> {
 			if(minAge.and(maxAge).test(emp)) {
-				System.out.println(formatEmpNoAge.apply(emp));
+				System.out.println(formatEmpWithAge.apply(formatEmpNoAge.apply(emp), emp));
 			}
 		});
 	}	
