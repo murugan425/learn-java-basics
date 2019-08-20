de9e7e58ad08b5bc34be0e24c9ca04a6b2044e24
ATPIAD1\atpsmnn <mnagarajan@atpco.net>
2019-08-20 06:51:13 -0400

    what actually happens in collectors.toList - you create a result type objet and start accumulating the results and combine them to provide the final output.

--- java-basics/src/com/tamil/learn/java/features/eight/streams/StreamExample6.java
+++ java-basics/src/com/tamil/learn/java/features/eight/streams/StreamExample6.java
@@ -6,10 +6,7 @@ package com.tamil.learn.java.features.eight.streams;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Supplier;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import org.apache.commons.lang3.RandomStringUtils;
 
@@ -45,7 +42,9 @@ public class StreamExample6 {
 			flatMap(dept -> dept.getEmployees().stream()).forEach(System.out::println);
 		
 		List<Employee> employees = departments.stream().flatMap(dept -> dept.getEmployees().stream()).
-				sorted((e1,e2)->e1.getDob().compareTo(e2.getDob())).collect(Collectors.toList());
+				sorted((e1,e2)->e1.getDob().compareTo(e2.getDob())).
+				collect(ArrayList::new, ArrayList::add, ArrayList::addAll); 
+				//[supplier - new result type, accumulator - collects the results, combiner]
 		System.out.println("Totally, " + employees.size() + " Employees generated");
 		employees.forEach(System.out::println);			
 	}
