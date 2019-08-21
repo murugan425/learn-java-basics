f719f7aa59ca873628bcccbd44513b6d72e1dd1a
ATPIAD1\atpsmnn <mnagarajan@atpco.net>
2019-08-21 05:28:01 -0400

    difference between thread.start and thread.run.

--- java-basics/src/com/tamil/learn/java/basics/concurrency/ThreadsExample1.java
+++ java-basics/src/com/tamil/learn/java/basics/concurrency/ThreadsExample1.java
@@ -16,9 +16,11 @@ public class ThreadsExample1 {
 	public static void main(String[] args) {
 		System.out.println(ANSI_BLUE+"Main class starts & trigger the threads");
 		PrimaryThread thread1 = new PrimaryThread();
+		thread1.setName("Primary Thread");
 		SecondThread thread2 = new SecondThread();
-		thread1.start();
-		thread2.start();
+		thread2.setName("Second Thread");
+		thread1.start(); //Primary thread instance is executed
+		thread2.run(); //Runs in the main thread not on a new thread instance
 		//thread1.start(); - java.lang.IllegalThreadStateException
 		Thread thread3 = new Thread(new PrimaryRunnable());
 		thread3.start();
